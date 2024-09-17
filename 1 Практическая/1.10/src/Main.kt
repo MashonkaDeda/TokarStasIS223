6df81632022c9ef7d6ce995a40688ef96c2d12da
DedushkinaMashonka <huaweyyflow@gmail.com>
2024-09-18 02:18:48 +0900

    Add files via upload

---
+++ 1 Практическая/1.10/src/Main.kt
@@ -0,0 +1,5 @@
+//1 практика 10
+fun main() {
+    println("50")
+    println("10")
+}
---
+++ 1 Практическая/1.8/src/Main.kt
@@ -0,0 +1,7 @@
+//1 практика 8
+fun main() {
+    val number1 = 15
+    val number2 = 30
+    val number3 = 45
+    println("$number1  $number2  $number3")
+}
